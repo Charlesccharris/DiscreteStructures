b90d863104310d5313c582aabeee6a53a8dcbce5
Charlesccharris <harriscc@student.swosu.edu>
2021-02-09 16:16:56 -0600

    Created problems 2 and 3, 3 worked, but now it doesn't

---
+++ Project/ChapterOne/Problem3.py
@@ -0,0 +1,25 @@
+#Assume a runner runs 24 miles in 1 hour, 40 minutes, and 35 seconds.
+#Write a program that displays the average speed in kilometers per hour.
+#(1 mile is 1.6 kilometers)
+
+time = [1, 40, 35]
+hours, minutes, seconds = time
+miles = 24
+
+kilometersPerMile = 1.6
+secondsInMinute = 60
+minutesInHour = 60
+
+kilometers = miles*kilometersPerMile
+kilometers = round(kilometers, 2)
+
+minutes += (seconds/secondsInMinute)
+minutes = round(minutes, 2)
+
+hours += (minutes/minutesInHour)
+hours = round(hours, 2
+
+variable = kilometers/hours
+
+print("What is the average speed in kilometers per hour, if you go 24 miles in 1 hour, 40 minutes, and 35 seconds?")
+print(kph)
