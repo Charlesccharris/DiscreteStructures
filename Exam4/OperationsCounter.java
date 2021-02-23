2a64ba7bb944a86e069d14c144fbae7568206915
Charlesccharris <harriscc@student.swosu.edu>
2021-02-23 15:12:15 -0600

    Just changed the order in which the methods were listed

--- Exam4/OperationsCounter.java
+++ Exam4/OperationsCounter.java
@@ -1,31 +1,57 @@
 class OperationsCounter{
-
+/*	--------------------------------------------
+			OperationsCounter
+	--------------------------------------------
+	-ifCount: int
+	-arithmiticCount: int
+	-startTime: long
+	-stopTime: long
+	-assignmentCount: long
+	--------------------------------------------
+	+increaseIf(): void
+	+increaseArithmitic(operations: int): void
+	+increaseAssignmentOperations(i: int): void
+	+getIfCount(): int
+	+getAddCount(): int
+	+getAssignment(): int
+	+resetCount(): void
+	+startTimer(): void
+	+stopTimer(): void
+	+getTime(): long
+	--------------------------------------------
+*/
 	private int ifCount = 0;
 	private int arithmiticCount = 0;
 	private long startTime = 0;
 	private long stopTime = 0;
 	private int assignmentCount = 0;
 
-	OperationsCounter(){
-
-	}
 	public void increaseIf(){
 		this.ifCount++;
 	}
+	public void increaseArithmitic(int operations){
+		this.arithmiticCount += operations;
+	}
+	public void increaseAssignmentOperations(int i){
+		this.assignmentCount += i;
+	}
+
 	public int getIfCount(){
 		return ifCount;
-	}
-	public void resetCount(){
-		this.ifCount = 0;
-		this.arithmiticCount = 0;
-	}
-	public void increaseArithmitic(int numberOfOperations){
-		this.arithmiticCount += numberOfOperations;
 	}
 	public int getAddCount(){
 		return this.arithmiticCount;
 	}
+	public int getAssignment(){
+		return this.assignmentCount;
+	}
 
+	public void resetCount(){
+		this.ifCount = 0;
+		this.arithmiticCount = 0;
+	}
+
+	//Timer
 	public void startTimer(){
 		this.startTime = System.currentTimeMillis();
 	}
@@ -35,11 +61,5 @@ class OperationsCounter{
 	public long getTime(){
 		long time = (this.stopTime - this.startTime);
 		return time;
-	}
-	public void increaseAssignmentOperations(int i){
-		this.assignmentCount += i;
-	}
-	public int getAssignment(){
-		return this.assignmentCount;
 	}
 }
