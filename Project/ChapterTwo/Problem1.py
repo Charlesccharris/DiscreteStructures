f59c88dacc5964970dbb1a2ab4ece85127005993
Charlesccharris <harriscc@student.swosu.edu>
2021-02-25 15:23:36 -0600

    Created python file for Problem 2_1

---
+++ Project/ChapterTwo/Problem1.py
@@ -0,0 +1,5 @@
+#Convert Celsius to Fahrenheit
+celsius = int(input("Enter a degree in Celsius: "))
+fahrenheit = (9.0/5.0) * celsius + 32
+print("Celsius: ", celsius)
+print("Fahrenheit: ", fahrenheit)
