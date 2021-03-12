d27e42567b944b8c53021d08acd5f13938a5d375
Charlesccharris <harriscc@student.swosu.edu>
2021-03-12 11:08:07 -0600

    Just input the instructions

---
+++ Exam6/Exam6.java
@@ -0,0 +1,11 @@
+class Exam6{
+	public static void main(String[] args){
+/*		Write a program to demonstrate the number
+		of people needed to ensure that the probability
+		of least two of them have the same day of the year
+		as their birthday is at least
+		70%, 80%, 90%, 95%, 98%, and at least 99%.
+*/
+		System.out.println(44);
+	}
+}
