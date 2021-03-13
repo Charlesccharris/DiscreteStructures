506816a17c09e0808071fa3e6434a1dcb98361eb
Charlesccharris <harriscc@student.swosu.edu>
2021-03-12 20:42:15 -0600

    Finished the program

--- Exam6/Exam6.java
+++ Exam6/Exam6.java
@@ -1,3 +1,4 @@
+import java.lang.Math;
 class Exam6{
 	public static void main(String[] args){
 /*		Write a program to demonstrate the number
@@ -6,6 +7,51 @@ class Exam6{
 		as their birthday is at least
 		70%, 80%, 90%, 95%, 98%, and at least 99%.
 */
-		System.out.println(44);
+		boolean seventy = true;
+		boolean eighty = true;
+		boolean ninety = true;
+		boolean ninetyFive = true;
+		boolean ninetyEight = true;
+		boolean ninetyNine = true;
+
+		double probability = 0;
+		int people = 1;
+		int top = 0;
+		double division = 0;
+		double total = 1.0;
+
+		for(int i = 2; i <= 100; i++){
+			people++;
+			top =  367 - people;
+			division = top/366.0;
+			total = total * division;
+			probability = 1 - total;
+
+			if(seventy && probability >= .7){
+				seventy = false;
+				System.out.println("At 70%, the number of people is " + people);
+			}
+			else if(eighty && probability >= .8){
+				eighty = false;
+				System.out.println("At 80%, the number of people is " + people);
+			}
+			else if(ninety && probability >= .9){
+				ninety = false;
+				System.out.println("At 90%, the number of people is " + people);
+			}
+			else if(ninetyFive && probability >=.95){
+				ninetyFive = false;
+				System.out.println("At 95%, the number of people is " + people);
+			}
+			else if(ninetyEight && probability >=.98){
+				ninetyEight = false;
+				System.out.println("At 98%, the number of people is " + people);
+			}
+			else if(ninetyNine && probability >=.99){
+				ninetyNine = false;
+				System.out.println("At 99%, the number of people is " + people);
+			}
+		}
 	}
 }
+
