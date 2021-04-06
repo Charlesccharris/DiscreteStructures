86f3509d181eb3a7455d065ce9fac63083de3dee
Charlesccharris <harriscc@student.swosu.edu>
2021-04-06 10:58:31 -0500

    Added some custom methods, and started working on the bubble sort

--- Exam8/Exam8.java
+++ Exam8/Exam8.java
@@ -14,17 +14,44 @@ class Exam8{
 		number = input.nextInt();
 		int[] list = new int[number];
 
-		for(int x = 0; x < number; x ++){
-			list[x] = (int)(Math.random()*100);
-		}
-		for(int x = 0; x < number; x ++){
-			System.out.println(list[x]);
-		}
-		opNumber = bubbleSort(list);
+		list = fillArray(list,number);
+		displayArray(list, number);
+
+		opNumber = bubbleSort(list, number);
 		opNumber = quickSort(list);
 	}
 
-	public static int bubbleSort(int[] list){
+	public static int[] fillArray(int[] list, int number){
+		for(int x = 0; x < number; x ++){
+			list[x] = (int)(Math.random()*100);
+		}
+		return list;
+	}
+
+	public static void displayArray(int[] list, int number){
+		for(int x = 0; x < number; x ++){
+			System.out.println(list[x]);
+		}
+	}
+
+	public static int bubbleSort(int[] list, int number){
+		int num = 0;
+		int numPos = 0;
+		boolean outOfPosition = true;
+		while(outOfPosition){
+			outOfPosition = false;
+			numPos = 
+			for(int x = 0; x < number; x++){
+				numPos = x;
+				num = list[x];
+				if(list[x] > num){
+					outOfPosition = true;
+					tempPos
+					tempNum
+				}
+			}
+		}
+		displayArray(list, number);
 		return 0;
 	}
 
